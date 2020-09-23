eaa91f74633dc964f688e1fd151e255a25bd18f2
Dahyunspec <66265683+Dahyunspec@users.noreply.github.com>
2020-09-23 13:16:27 -0500

    v1.3
    final final

--- js/carousel-team.js
+++ js/carousel-team.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#team .team').slick({
-      slidesToShow: 6,
+      slidesToShow: 4,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 1000,
