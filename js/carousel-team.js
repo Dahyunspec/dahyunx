ec460ec97c251333e53b3ac94fc53ee235558e62
Dahyunspec <66265683+Dahyunspec@users.noreply.github.com>
2020-09-22 19:33:10 -0500

    v1.1
    final

---
+++ js/carousel-team.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('#team .team').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      centerMode: true,
+      variableWidth: true,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });
+  });
---
+++ js/nivo.js
@@ -0,0 +1,16 @@
+
+// Hide nav on click
+$(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  }); 
+	
+	
+    // Nivo Lightbox 
+    $('.portfolio-item a').nivoLightbox({
+            effect: 'slideDown',  
+            keyboardNav: true,                            
+        }); 
